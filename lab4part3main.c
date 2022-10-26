706b974d32887100532911c26d6903d5f913be9a
drake6790 <54121274+drake6790@users.noreply.github.com>
2022-10-26 14:21:47 -0400

    Add files via upload

---
+++ lab4part3main.c
@@ -0,0 +1,16 @@
+#include <msp430.h>
+int main(void)
+{
+    WDTCTL = WDT_ADLY_250;                      // WDT 1000ms, SMCLK, interval timer
+    P1OUT &= ~BIT0;
+    P1DIR |= BIT0;
+    PM5CTL0 &= ~LOCKLPM5;
+    SFRIE1 |= WDTIE;
+    _enable_interrupts();
+    while (1);
+    }
+    #pragma vector=WDT_VECTOR
+    __interrupt void wdtled(void)
+    {
+            P1OUT^=BIT0;
+    }
